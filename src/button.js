e180dbb8c3dfa30dbaf288cc2a24acee2861ba55
Mikhail Pshenichnyy <dremast1337@gmail.com>
2023-10-23 18:44:22 +0300

    updated age counter

--- src/button.js
+++ src/button.js
@@ -1,18 +1,20 @@
 // import {buttonListener} from "./script.js";
 
- 
-const main = document.querySelector('.main')
 
- 
+const main = document.querySelector('.main')
+const age = document.querySelector('#age')
+
+const myAge = new Date(Date.now() - new Date('2005-08-22').getTime()).getFullYear() - 1970;
+age.textContent = myAge;
 
 function hideInfo() {
- 
+
   main.classList.add('main_invisible')
   main.classList.remove('main_visible')
 }
 
 function showInfo() {
- 
+
   main.classList.remove('main_invisible')
   main.classList.add('main_visible')
 }
